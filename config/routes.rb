55bcde0a2b91558f692fe4dc3a7cb5148f8343f6
puifaiba <puifai.ba@gmail.com>
2021-04-13 20:28:36 -0400

    user and sessions controller

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  resources :users, only: [:create, :show, :index]
+
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get 'logged_in', to: 'sessions#is_logged_in?'
+end
