470d4036925198294cbc93f603cabc833236e1b4
puifaiba <puifai.ba@gmail.com>
2021-04-20 01:53:42 -0400

    able to add task

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,12 +9,6 @@ user2 = User.create!(email: "choppo@test.com", password: "password2", first_name
 user3 = User.create!(email: "ravi@test.com", password: "password3", first_name: "Ravi", last_name: "Wavi", role: 0, therapist_id: user1.id)
 user4 = User.create!(email: "pepe@test.com", password: "password4", first_name: "Pepe", last_name: "Wepe", role: 0, therapist_id: user2.id)
 
-# task1 = Task.create!(name: "Final Project", description: "A Rails/React app", due_date: Time.now, status: "In progress", user_id: user3.id)
-# task2 = Task.create!(name: "Final Project", description: "A Rails/React app", due_date: Time.now, status: "Done", user_id: user4.id)
-
-# interval1 = Interval.create!(date: Time.now, focus_duration: 25, break_duration: 5, long_break_duration: 15, category: "work", on_task: true, comment: "completed on time", task_id: task1.id)
-# interval2 = Interval.create!(date: Time.now, focus_duration: 30, break_duration: 15, long_break_duration: 30, category: "study", on_task: false, diversion: "social media", comment: "completed on time", task_id: task2.id)
-
 chat1 = Chat.create!(name: "Ravi")
 chat2 = Chat.create!(name: "Pepe")
 
